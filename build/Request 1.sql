cc85b4e377e82179076e7b3788b2d25bf438f56e
Vijaya Mense <vijayamense@gmail.com>
2025-07-25 14:27:02 +0530

    Delete build directory

--- build/Request 1.sql
+++
@@ -1,6 +0,0 @@
-/*1. Provide the list of markets in which customer "Atliq Exclusive" operates its business in the APAC region.*/
-
-SELECT market FROM gdb023.dim_customer
-where customer="Atliq Exclusive" and region="APAC"
-group by market
-order by market;
